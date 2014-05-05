99fcee1792b6f27dabe4f25089d1c228ca367735
Joel Purra <code+github@joelpurra.com>
2014-05-05 17:50:31 +0200

    Build a placeholder HAR file with an error comment instead of a plain error message string output

--- src/get/har.sh
+++ src/get/har.sh
@@ -11,7 +11,14 @@ phantomRelativeLink=$(readlink "$phantomPath")
 phantomRealpath=`echo $phantomDir/$phantomRelativeLink`
 phantomRealdir=$(dirname "$phantomRealpath")
 netsniffJs="$phantomRealdir/../share/phantomjs/examples/netsniff.js"
+executionErrorHAR="./execution-error.har"
 
-phantomjs "$netsniffJs" "$url"
+result=$(phantomjs "$netsniffJs" "$url")
+
+if [[ $? == 0 ]]; then
+	echo "$result"
+else
+	cat "$executionErrorHAR" | jq --arg url "$url" --arg error "$result" '. | .log.comment = "There was an error downloading \($url)\n\($error)"'
+fi
 
 cd - > /dev/null
