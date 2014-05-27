b3641add7037dc8571f5400c82f8045315d0df31
Joel Purra <code+github@joelpurra.com>
2014-05-27 14:14:27 +0200

    Parallel HAR batch downloading

---
+++ src/domain/parallel.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+domainsfile="$PWD/$1"
+
+cd "$(dirname $0)"
+
+counter=0
+parallelLimit=100
+
+timestamp(){
+	now=$(date -u +%FT%TZ)
+	echo "$now"
+}
+
+while read domain; do
+	./single.sh "$domain" &
+	((counter++))
+
+	for i in $(seq 1 "$((parallelLimit-1))"); do
+		read domain && ./single.sh "$domain" &
+		((counter++))
+   done
+   wait
+   echo -n "$counter - "
+   timestamp
+done < "$domainsfile"
+
+cd - > /dev/null
