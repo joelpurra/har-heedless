d5a8a2dbe66ac9d1bda8f3303102f262fb7da92f
Joel Purra <code+github@joelpurra.com>
2014-07-15 13:22:30 +0200

    Remove counter and timestamp from output

--- src/domain/parallel.sh
+++ src/domain/parallel.sh
@@ -6,23 +6,13 @@ domainsfile="$1"
 enableScreenshot=false
 [[ ("$2" == "--screenshot") && ("$3" == "true") ]] && enableScreenshot=true
 
-counter=0
 parallelLimit=100
-
-timestamp(){
-	now=$(date -u +%FT%TZ)
-	echo "$now"
-}
 
 while read domain; do
 	"${BASH_SOURCE%/*}/single.sh" "$domain" --screenshot "$enableScreenshot" &
-	((counter++))
 
 	for i in $(seq 1 "$((parallelLimit-1))"); do
 		read domain && "${BASH_SOURCE%/*}/single.sh" "$domain" --screenshot "$enableScreenshot" &
-		((counter++))
 	done
 	wait
-	echo -n "$counter - "
-	timestamp
 done < "$domainsfile"
