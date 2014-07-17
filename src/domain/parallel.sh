aa787ea770b999ae9011f57f715cd70524d70cd2
Joel Purra <code+github@joelpurra.com>
2014-07-17 11:23:35 +0200

    Use GNU parallel for improved CPU load handling, output domain as a progress indicator

--- src/domain/parallel.sh
+++ src/domain/parallel.sh
@@ -6,11 +6,5 @@ parallelLimit="${1:-10}"
 enableScreenshot=false
 [[ ("$2" == "--screenshot") && ("$3" == "true") ]] && enableScreenshot=true
 
-while read domain; do
-	"${BASH_SOURCE%/*}/single.sh" "$domain" --screenshot "$enableScreenshot" &
+cat | parallel --jobs "$parallelLimit" --load "80%" --line-buffer "echo \"{}\"; \"${BASH_SOURCE%/*}/single.sh\" \"{}\" --screenshot \"$enableScreenshot\";"
 
-	for i in $(seq 1 "$((parallelLimit-1))"); do
-		read domain && "${BASH_SOURCE%/*}/single.sh" "$domain" --screenshot "$enableScreenshot" &
-	done
-	wait
-done
