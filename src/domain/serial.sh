555fa3b72add5b09f51b6e28dbe92751cbcee55b
Joel Purra <code+github@joelpurra.com>
2014-07-17 11:22:38 +0200

    Add the current domain as a progress indicator

--- src/domain/serial.sh
+++ src/domain/serial.sh
@@ -5,5 +5,6 @@ enableScreenshot=false
 [[ ("$1" == "--screenshot") && ("$2" == "true") ]] && enableScreenshot=true
 
 while read domain; do
+	echo "$domain"
 	"${BASH_SOURCE%/*}/single.sh" "$domain" --screenshot "$enableScreenshot"
 done
